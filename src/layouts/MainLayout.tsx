624346a66d9fc240b0acbb524f9061db9d0c4672
wafaa <wafaaatta04@gmail.com>
2024-09-27 14:27:42 +0300

    installer pusher js et initialiser la connexion

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,8 +2,25 @@ import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Notification from '../components/Notification'
+import Pusher from 'pusher-js'
+import { useEffect } from 'react'
 
 const MainLayout = () => {
+  const pusher = new Pusher('c54be6c2bdbc371de2e5', {
+    cluster: 'mt1',
+  });
+
+  useEffect(() => {
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      alert(JSON.stringify(data));
+    });
+
+    return () => {
+      pusher.unsubscribe('my-channel');
+    }
+  }, [pusher])
+
   return (
     <div className="flex flex-col min-h-screen bg-slate-100 custom-scroll">
       <Header />
